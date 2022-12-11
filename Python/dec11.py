f210b3b4a6e3379c3dc0508482ed70d25a4f75fd
alakhpandya <alakhpandya@gmail.com>
2022-12-11 15:04:28 +0530

    Create dec11.py

---
+++ Python/dec11.py
@@ -0,0 +1,37 @@
+print("Hello World!")	# in-line comments
+
+"""
+Multi
+line
+comment
+"""
+print('Hello Python!', 'How are you?')
+print('Hello Python!'        'How are you?')
+
+'''
+1. Alpha said Beta 'Gamma is teasing Delta'
+2. Alpha said Beta 'Gamma is teasing Delta but Gamma says "I didn't tease Delta." Now what to do?'
+'''
+print("Alpha said Beta 'Gamma is teasing Delta but Gamma", 'says "I', "didn't tease", 'Delta." Now what to', "do?'")
+
+"""
+Escape sequence characters:
+\
+\n	new line character
+\t	tab character	
+"""
+
+print('Alpha said Beta \'Gamma is teasing Delta\'')
+
+"""
+3. Location of your Python interpreter is at: "C:\new folder\temp\python\"
+4. In Python, we use '\t' to give a "tab". But, if we want Python to ignore it, we type "\\t"
+"""
+
+
+
+
+
+
+
+
