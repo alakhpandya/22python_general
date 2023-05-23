536142d82f20c05f03404bc4a5cdd1542ab43a6c
alakhpandya <alakhpandya@gmail.com>
2023-05-23 19:58:51 +0530

    Create may21.py

---
+++ Python/may21.py
@@ -0,0 +1,12 @@
+# Ask an integer from user and print that term of Fibonacci sequence using recursive function that takes an integer 'n' as argument and returns nth term of Fibonacci Sequance. (n >= 1)
+"""
+1, 1, 2, 3, 5, 8, 13........
+recursive formula:
+nth term = (n-1)th term + (n-2)th term
+"""
+def fibo(n):
+    pass
+    # Write your code here
+
+n = int(input())
+print(fibo(n))
