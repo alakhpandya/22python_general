038c627bf84c59dbf552b10333ed28f8f77d8319
alakhpandya <alakhpandya@gmail.com>
2023-07-31 19:25:57 +0530

    Update file_handling.py

--- Python/IMS/Concepts/file_handling.py
+++ Python/IMS/Concepts/file_handling.py
@@ -1,4 +1,4 @@
-f = open("C:\\Users\\Alakh Pandya\\Desktop\\Batches\\22python_general\\Python\\IMS\\Concepts\\ourBatch.txt")
+# f = open("C:\\Users\\Alakh Pandya\\Desktop\\Batches\\22python_general\\Python\\IMS\\Concepts\\ourBatch.txt")
 # data = f.read()
 # print(type(data))
 # print(data)
@@ -23,6 +23,61 @@ f = open("C:\\Users\\Alakh Pandya\\Desktop\\Batches\\22python_general\\Python\\I
 # print(l)
 # print(m)
 
-n = f.readlines()
-print(n)
+# n = f.readlines()
+# print(n)
+# f.close()
+
+"""
+f = open("D:\\ourBatch.txt")
+data = f.readlines()
+# print(data)
 f.close()
+
+roll = int(input("Roll no: "))
+stu_info = data[roll]
+
+
+f = open("C:\\Users\\Alakh Pandya\\Desktop\\Batches\\22python_general\\Python\\IMS\\Concepts\\ourBatch.txt")
+
+# d1 = f.readlines()
+# print(f.read())
+# f.write("\n12")
+# print(f.writable())
+# print(f.readable())
+
+f.close()
+"""
+
+# File opening syntax:
+"""
+f = open('entire file path including its name with extension', 'Mode1Mode2')
+f.close()
+
+Mode 1  Name    Description                                             Mode 2  Name
+r       Read    Opens the file for reading only                         t       Text            Default Mode
+                Cursor is placed at the begining of the file.
+
+w       Write   Opens the file for writing only                         b       Binary
+
+a       Append  Opens the file for writing only
+x       
+r+      Read+
+w+      Write+
+a+      Append+
+
+"""
+
+# f = open("D:\\ourBatch.txt")        OR
+f = open("D:\\ourBatch.txt", "rt")
+
+# f = open("D:\\ourBatch.txt", "w")   OR
+# f = open("D:\\ourBatch.txt", "wt")
+
+# f = open("D:\\ourBatch.txt", "at")      OR
+# f = open("D:\\ourBatch.txt", "a")
+
+# f = open("D:\\ourBatch.txt", "a+b")
+# f = open("D:\\ourBatch.txt", "a+")    # as good as "a + t"
+f.close()
+
+# Next Class: Remaining concepts of file handling, Resolving '\t' problem, back to project
